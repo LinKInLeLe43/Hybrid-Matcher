0227293b82f088d46bd303d9b3d58d17c637834c
LinKInLeLe43 <18672552240@163.com>
2025-02-24 19:36:53 +0800

    test attention

--- src/models/components/nets/new_matcher/attention.py
+++ src/models/components/nets/new_matcher/attention.py
@@ -1,8 +1,8 @@
 from typing import Optional
 
 import torch
-from torch import nn
-from torch.nn import functional as F
+import torch.nn as nn
+import torch.nn.functional as F
 
 if hasattr(F, "scaled_dot_product_attention"):
     FLASH_AVAILABLE = True
@@ -12,10 +12,12 @@ else:
 
 
 class Attention(nn.Module):
-    def __init__(self, try_sdpa: bool = False, try_flash: bool = False) -> None:
+    def __init__(
+        self, allow_sdp: bool = False, force_flash: bool = False
+    ) -> None:
         super().__init__()
-        self.enable_sdpa = try_sdpa and FLASH_AVAILABLE
-        self.enable_flash = try_flash and self.enable_sdpa
+        self.enable_sdp = allow_sdp and FLASH_AVAILABLE
+        self.force_flash = force_flash
 
     def forward(
         self,
@@ -23,29 +25,33 @@ class Attention(nn.Module):
         k: torch.Tensor,
         v: torch.Tensor,
         q_mask: Optional[torch.Tensor] = None,
-        kv_mask: Optional[torch.Tensor] = None
+        kv_mask: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         mask = None
         if q_mask is not None and kv_mask is not None:
             mask = q_mask[..., :, None] & kv_mask[..., None, :]
 
-        if self.enable_sdpa:
-            if self.enable_flash:
+        if self.enable_sdp:
+            if self.force_flash:
                 args = [x.contiguous().half() for x in [q, k, v]]
                 with sdp_kernel(
-                    enable_math=False, enable_flash=True,
-                    enable_mem_efficient=False):
+                    enable_math=False,
+                    enable_flash=True,
+                    enable_mem_efficient=False,
+                ):
                     out = F.scaled_dot_product_attention(
-                        *args, attn_mask=mask).to(q.dtype)
+                        *args, attn_mask=mask
+                    ).to(q.dtype)
             else:
                 args = [x.contiguous() for x in [q, k, v]]
                 out = F.scaled_dot_product_attention(*args, attn_mask=mask)
         else:
-            sc = q.shape[-1]
-            similarity = torch.einsum("...ld,...sd->...ls", q, k) / sc ** 0.5
+            scale = q.shape[-1] ** -0.5
+            similarity = scale * torch.einsum("...ld,...sd->...ls", q, k)
             if mask is not None:
                 similarity.masked_fill_(~mask, -1e9)
 
             out = torch.einsum(
-                "...ls,...sc->...lc", F.softmax(similarity, dim=-1), v)
+                "...ls,...sc->...lc", F.softmax(similarity, dim=-1), v
+            )
         return out
