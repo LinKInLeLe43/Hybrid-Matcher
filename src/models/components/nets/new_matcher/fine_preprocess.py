0e8b5bc24d56b507659620251db0d66b370ef0e1
LinKInLeLe43 <18672552240@163.com>
2025-04-20 19:15:52 +0800

    refactor fine_preprocess.py

--- src/models/components/nets/new_matcher/fine_preprocess.py
+++ src/models/components/nets/new_matcher/fine_preprocess.py
@@ -1,153 +1,109 @@
+# TODO:
+# - Rename
+# - Change weight init
+# - Check `align_corners` in `fpn_fuse`
+# - Remove `upscale_before_crop`
+
 from typing import List, Tuple
 
 import torch
-from torch import nn
-from torch.nn import functional as F
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
 
 
 class FinePreprocess(nn.Module):
     def __init__(
         self,
-        type: str,
+        dims: List[int],
         window_size: int,
         stride: int,
         padding: int,
-        depths: List[int],
         right_extra: int = 0,
-        scale_before_crop: int = 1,
-        norm_before_fuse: bool = False
+        upscale_before_crop: float = 1.0,
     ) -> None:
         super().__init__()
-        self.type = type
-        self.window_size = window_size
         self.stride = stride
-        self.padding = padding
-        self.depths = depths
-        self.right_extra = right_extra
-        self.scale_before_crop = scale_before_crop
-        self.norm_before_fuse = norm_before_fuse
+        self.upscale_before_crop = upscale_before_crop
 
-        if type == "loftr":
-            self.proj = nn.Linear(depths[-1], depths[0])
-            self.merge = nn.Linear(2 * depths[0], depths[0])
-        elif type == "eloftr":
-            self.ups, self.downs = nn.ModuleList(), nn.ModuleList()
-            for i in range(len(depths[:-1])):
-                c0, c1 = depths[i], depths[i + 1]
-                self.ups.append(nn.Conv2d(c0, c1, 1, bias=False))
-                self.downs.append(nn.Sequential(
-                    nn.Conv2d(c1, c1, 3, padding=1, bias=False),
-                    nn.BatchNorm2d(c1),
+        self.window_size0 = window_size
+        self.window_size1 = window_size + 2 * right_extra
+        self.padding0, self.padding1 = padding, padding + right_extra
+
+        self.ups, self.downs = nn.ModuleList(), nn.ModuleList()
+        for i in range(len(dims) - 1):
+            dim0, dim1 = dims[i], dims[i + 1]
+            self.ups.append(nn.Conv2d(dim0, dim1, 1, bias=False))
+            self.downs.append(
+                nn.Sequential(
+                    nn.Conv2d(dim1, dim1, 3, padding=1, bias=False),
+                    nn.BatchNorm2d(dim1),
                     nn.LeakyReLU(inplace=True),
-                    nn.Conv2d(c1, c0, 3, padding=1, bias=False)))
-            self.ups.append(nn.Conv2d(c1, c1, 1, bias=False))
-        else:
-            raise ValueError("")
+                    nn.Conv2d(dim1, dim0, 3, padding=1, bias=False),
+                )
+            )
+        self.ups.append(nn.Conv2d(dim1, dim1, 1, bias=False))
 
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.kaiming_normal_(p, mode="fan_out", nonlinearity="relu")
 
-    def _crop_by_idxes(
+    def crop_by_indices(
         self,
         x0: torch.Tensor,
         x1: torch.Tensor,
-        idxes: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+        index_triplet: Tuple[torch.Tensor, torch.Tensor, torch.Tensor],
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        s, e, scale = self.stride, self.right_extra, self.scale_before_crop
-        w0, w1 = self.window_size, self.window_size + 2 * e
-        p0, p1 = self.padding, self.padding + e
-        ww0, ww1 = w0 ** 2, w1 ** 2
-        b_idxes, i_idxes, j_idxes = idxes
+        w0, w1 = self.window_size0, self.window_size1
+        upscale = self.upscale_before_crop
+        b_indices, i_indices, j_indices = index_triplet
 
-        if scale > 1:
-            x0 = F.interpolate(x0, scale_factor=scale, mode="bilinear")
-            x1 = F.interpolate(x1, scale_factor=scale, mode="bilinear")
+        if upscale > 1.0:
+            x0 = F.interpolate(
+                x0, scale_factor=upscale, mode="bilinear", align_corners=False
+            )
+            x1 = F.interpolate(
+                x1, scale_factor=upscale, mode="bilinear", align_corners=False
+            )
 
-        out0 = F.unfold(x0, w0, padding=p0, stride=s)[b_idxes, :, i_idxes]
-        out1 = F.unfold(x1, w1, padding=p1, stride=s)[b_idxes, :, j_idxes]
-        out0 = out0.unflatten(1, (-1, ww0)).transpose(1, 2)
-        out1 = out1.unflatten(1, (-1, ww1)).transpose(1, 2)
-        return out0, out1
+        cropped0 = F.unfold(x0, w0, stride=self.stride, padding=self.padding0)[
+            b_indices, :, i_indices
+        ]
+        cropped1 = F.unfold(x1, w1, stride=self.stride, padding=self.padding1)[
+            b_indices, :, j_indices
+        ]
+        cropped0 = rearrange(cropped0, "m (c ww) -> m ww c", ww=w0**2)
+        cropped1 = rearrange(cropped1, "m (c ww) -> m ww c", ww=w1**2)
+        return cropped0, cropped1
 
-    def _fuse_loftr(
-        self,
-        x0s: List[torch.Tensor],
-        x1s: List[torch.Tensor],
-        idxes: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        e = self.right_extra
-        w0, w1 = self.window_size, self.window_size + 2 * e
-        ww0, ww1 = w0 ** 2, w1 ** 2
-        b_idxes, i_idxes, j_idxes = idxes
-
-        x0 = x0s[-1].flatten(start_dim=2).transpose(1, 2)
-        x1 = x1s[-1].flatten(start_dim=2).transpose(1, 2)
-        y0, y1 = x0s[0], x1s[0]
-        x = torch.cat([x0[b_idxes, i_idxes], x1[b_idxes, j_idxes]])
-        x = self.proj(x)[:, None]
-        y0, y1 = self._crop_by_idxes(y0, y1, idxes)
-
-        if e == 0:
-            x = x.expand(-1, ww0, -1)
-            y = torch.cat([y0, y1])
-            y = torch.cat([y, x], dim=2)
-            out0, out1 = self.merge(y).chunk(2)
-        else:
-            x0, x1 = x.chunk(2)
-            x0, x1 = x0.expand(-1, ww0, -1), x1.expand(-1, ww1, -1)
-            y0, y1 = torch.cat([y0, x0], dim=2), torch.cat([y1, x1], dim=2)
-            out0, out1 = self.merge(y0), self.merge(y1)
-        return out0, out1
-
-    def _fuse_eloftr_impl(self, xs: List[torch.Tensor]) -> torch.Tensor:
-        xs[-1] = self.ups[-1](xs[-1])
-        for i in reversed(range(len(xs[:-1]))):
-            xs[i] = self.ups[i](xs[i])
-            xs[i] += F.interpolate(
-                xs[i + 1], scale_factor=2.0, mode="bilinear",
-                align_corners=True)
-            xs[i] = self.downs[i](xs[i])
-        return xs[0]
-
-    def _fuse_eloftr(
-        self,
-        x0s: List[torch.Tensor],
-        x1s: List[torch.Tensor],
-        idxes: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        if x0s[0].shape == x1s[0].shape:
-            xs = [torch.cat(x) for x in zip(x0s, x1s)]
-            out0, out1 = self._fuse_eloftr_impl(xs).chunk(2)
-        else:
-            out0 = self._fuse_eloftr_impl(x0s)
-            out1 = self._fuse_eloftr_impl(x1s)
-        out0, out1 = self._crop_by_idxes(out0, out1, idxes)
-        return out0, out1
+    def fpn_fuse(self, x_list: List[torch.Tensor]) -> torch.Tensor:
+        x = self.ups[-1](x_list[-1])
+        for i in reversed(range(len(x_list) - 1)):
+            x, y = self.ups[i](x_list[i]), x
+            # FIXME: align_corners=False
+            x = x + F.interpolate(
+                y, scale_factor=2.0, mode="bilinear", align_corners=True
+            )
+            x = self.downs[i](x)
+        return x
 
     def forward(
         self,
-        x0s: List[torch.Tensor],
-        x1s: List[torch.Tensor],
-        idxes: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+        x0_list: List[torch.Tensor],
+        x1_list: List[torch.Tensor],
+        index_triplet: Tuple[torch.Tensor, torch.Tensor, torch.Tensor],
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        ww0 = self.window_size ** 2
-        ww1 = (self.window_size + 2 * self.right_extra) ** 2
-        m, c = len(idxes[0]), self.depths[0]
+        x0, x1 = x0_list[0], x1_list[0]
 
-        if m == 0:
-            out0 = x0s[0].new_empty((0, ww0, c))
-            out1 = x1s[0].new_empty((0, ww1, c))
-            return out0, out1
+        if index_triplet[0].shape[0] == 0:
+            cropped0 = x0.new_empty(0, self.window_size0**2, x0.shape[1])
+            cropped1 = x1.new_empty(0, self.window_size1**2, x1.shape[1])
+            return cropped0, cropped1
 
-        if self.norm_before_fuse:
-            x0s[-1] = x0s[-1] / self.depths[-1] ** 0.5
-            x1s[-1] = x1s[-1] / self.depths[-1] ** 0.5
-
-        if self.type == "loftr":
-            out0, out1 = self._fuse_loftr(x0s, x1s, idxes)
-        elif self.type == "eloftr":
-            out0, out1 = self._fuse_eloftr(x0s, x1s, idxes)
+        if x0.shape == x1.shape:
+            x_list = [torch.cat(x) for x in zip(x0_list, x1_list)]
+            x0, x1 = self.fpn_fuse(x_list).chunk(2)
         else:
-            assert False
-        return out0, out1
+            x0, x1 = self.fpn_fuse(x0_list), self.fpn_fuse(x1_list)
+        cropped0, cropped1 = self.crop_by_indices(x0, x1, index_triplet)
+        return cropped0, cropped1
