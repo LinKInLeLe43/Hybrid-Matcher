04b677599a0c360f797ec45f92248c76fb1f7828
LinKInLeLe43 <18672552240@163.com>
2025-04-20 18:39:55 +0800

    refactor fine_matching.py

--- src/models/components/nets/new_matcher/fine_matching.py
+++ src/models/components/nets/new_matcher/fine_matching.py
@@ -1,110 +1,96 @@
+# TODO:
+# - Change output keys
+# - remove temperature, use self.scale
+
 from typing import Any, Dict
 
-import kornia as K
-from kornia import geometry
 import torch
-from torch import nn
-from torch.nn import functional as F
+import torch.nn as nn
+from kornia.geometry import spatial_expectation2d
+from kornia.utils import create_meshgrid
 
 
 class FineMatching(nn.Module):
     def __init__(
         self,
-        type: str,
-        depth: int,
+        name: str,
+        dim: int,
         window_size: int,
         temperature: float = 1.0,
         cls_offset: float = 0.5,
-        reg_by_exp_with_std: bool = False
     ) -> None:
         super().__init__()
-        self.type = type
-        self.depth = depth
+        self.name = name
         self.window_size = window_size
         self.temperature = temperature
-        self.cls_offset = cls_offset
+        self.cls_offset = cls_offset  # NOTE: used by matching_module.py
+        self.scale = dim**-0.5
 
-        w = window_size
-        if type == "classification":
-            grid = K.create_meshgrid(w, w, normalized_coordinates=False)
-            delta = (grid - w / 2 + cls_offset).reshape(-1, 2)
-            self.register_buffer("cls_delta", delta, persistent=False)
-        elif type == "regression_by_expectation":
-            self.reg_by_exp_with_std = reg_by_exp_with_std
+        if name == "classification":
+            grid = create_meshgrid(
+                window_size, window_size, normalized_coordinates=False
+            ).flatten(end_dim=-2)
+            cls_biases = grid - window_size / 2 + cls_offset
+            self.register_buffer("cls_biases", cls_biases, persistent=False)
+        elif name == "regression":
+            pass
         else:
             raise ValueError("")
 
-    def _compute_cls_biases(
-        self,
-        x0: torch.Tensor,
-        x1: torch.Tensor
+    def compute_cls_biases(
+        self, x0: torch.Tensor, x1: torch.Tensor
     ) -> Dict[str, Any]:
-        ww, c = self.window_size ** 2, self.depth
-        m = len(x0)
+        ww = self.window_size**2
 
-        if m == 0:
-            result = {
-                "fine_cls_heatmap": x0.new_empty((0, ww, ww)),
-                "fine_cls_idxes": 3 * (x0.new_empty((0,), dtype=torch.long),),
-                "fine_cls_biases0": x0.new_empty((0, 2)),
-                "fine_cls_biases1": x0.new_empty((0, 2))}
-            return result
+        if x0.shape[0] == 0:
+            out = {
+                "fine_cls_heatmap": x0.new_empty(0, ww, ww),
+                "fine_cls_idxes": 3 * (x0.new_empty(0, dtype=torch.long),),
+                "fine_cls_biases0": x0.new_empty(0, 2),
+                "fine_cls_biases1": x0.new_empty(0, 2),
+            }
+            return out
 
-        x0, x1 = x0 / c ** 0.5, x1 / c ** 0.5
-        similarity = torch.einsum("mlc,msc->mls", x0, x1)
-        similarity /= self.temperature
-        heatmap = F.softmax(similarity, dim=1) * F.softmax(similarity, dim=2)
+        x0, x1 = x0 * self.scale, x1 * self.scale
+        similarity = torch.einsum("mlc,msc->mls", x0, x1) / self.temperature
+        confidence = similarity.softmax(dim=-2) * similarity.softmax(dim=-1)
 
         with torch.no_grad():
-            m_idxes = torch.arange(m, device=x0.device)
-            idxes = heatmap.flatten(start_dim=1).argmax(dim=1)
-            idxes = m_idxes, idxes // ww, idxes % ww
-            biases0 = self.cls_delta.index_select(0, idxes[1])
-            biases1 = self.cls_delta.index_select(0, idxes[2])
+            m_indices = torch.arange(x0.shape[0], device=x0.device)
+            ij_indices = confidence.flatten(start_dim=-2).argmax(dim=-1)
+            i_indices, j_indices = ij_indices // ww, ij_indices % ww
+            biases0 = self.cls_biases[i_indices]
+            biases1 = self.cls_biases[j_indices]
 
-        result = {"fine_cls_heatmap": heatmap,
-                  "fine_cls_idxes": idxes,
-                  "fine_cls_biases0": biases0,
-                  "fine_cls_biases1": biases1}
-        return result
+        out = {
+            "fine_cls_heatmap": confidence,
+            "fine_cls_idxes": (m_indices, i_indices, j_indices),
+            "fine_cls_biases0": biases0,
+            "fine_cls_biases1": biases1,
+        }
+        return out
 
-    def _compute_reg_biases_by_expectation(
-        self,
-        x0: torch.Tensor,
-        x1: torch.Tensor
+    def compute_reg_biases(
+        self, x0: torch.Tensor, x1: torch.Tensor
     ) -> Dict[str, Any]:
-        w, c = self.window_size, self.depth
-        m, ww = len(x0), w ** 2
+        w = self.window_size
 
-        if m == 0:
-            result = {"fine_reg_biases": x0.new_empty((0, 2))}
+        if x0.shape[0] == 0:
+            out = {"fine_reg_biases": x0.new_empty(0, 2)}
+            return out
 
-            if self.reg_by_exp_with_std:
-                result["fine_reg_stds"] = x0.new_empty((0,))
-            return result
-
-        center0 = x0[:, ww // 2] if len(x0.shape) == 3 else x0
-        center0, x1 = center0 / c ** 0.25, x1 / c ** 0.25
-        similarity = torch.einsum("mc,mrc->mr", center0, x1)
-        similarity /= self.temperature
-        heatmap = F.softmax(similarity, dim=1).reshape(-1, w, w)
-        biases = geometry.spatial_expectation2d(heatmap[None])[0]
-        result = {"fine_reg_biases": biases}
-
-        if self.reg_by_exp_with_std:
-            with torch.no_grad():
-                grid = K.create_meshgrid(w, w, device=x0.device)
-                vars = ((heatmap[..., None] * grid ** 2).sum(dim=(1, 2)) -
-                        biases ** 2)
-                stds = vars.clamp(min=1e-10).sqrt().sum(dim=1)
-            result["fine_reg_stds"] = stds
-        return result
+        x0 = x0[:, w**2 // 2] * self.scale
+        similarity = torch.einsum("mc,mrc->mr", x0, x1) / self.temperature
+        confidence = similarity.softmax(dim=-1).unflatten(-1, (w, w))
+        biases = spatial_expectation2d(confidence[None])[0]
+        out = {"fine_reg_biases": biases}
+        return out
 
     def forward(self, x0: torch.Tensor, x1: torch.Tensor) -> Dict[str, Any]:
-        if self.type == "classification":
-            result = self._compute_cls_biases(x0, x1)
-        elif self.type == "regression_by_expectation":
-            result = self._compute_reg_biases_by_expectation(x0, x1)
+        if self.name == "classification":
+            out = self.compute_cls_biases(x0, x1)
+        elif self.name == "regression":
+            out = self.compute_reg_biases(x0, x1)
         else:
-            assert False
-        return result
+            raise AssertionError("")
+        return out
